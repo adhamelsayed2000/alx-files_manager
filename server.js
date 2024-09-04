97476e5f35944efc1cc2a67536d1410afa75f043
adhamelsayed2000 <adhamadhoom9@gamil.com>
2024-09-04 20:05:44 +0300

    add fixed

--- server.js
+++ server.js
@@ -1,13 +1,12 @@
 import express from 'express';
-import router from './routes';
+import controllerRouting from './routes/index';
 
 const app = express();
-
 const port = process.env.PORT || 5000;
 
 app.use(express.json());
 
-app.use('/', router);
+controllerRouting(app);
 
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
--- utils/db.js
+++ utils/db.js
@@ -1,87 +1,55 @@
-import { MongoClient, ObjectId } from 'mongodb';
+import { MongoClient } from 'mongodb';
 
+const DB_HOST = process.env.DB_HOST || 'localhost';
+const DB_PORT = process.env.DB_PORT || 27017;
+const DB_DATABASE = process.env.DB_DATABASE || 'files_manager';
+const url = `mongodb://${DB_HOST}:${DB_PORT}`;
+
+/**
+ * Class for performing operations with Mongo service
+ */
 class DBClient {
   constructor() {
-    const host = process.env.DB_HOST || 'localhost';
-    const port = process.env.DB_PORT || '27017';
-    const database = process.env.DB_DATABASE || 'files_manager';
-    const url = `mongodb://${host}:${port}`;
-    this.client = new MongoClient(url, { useUnifiedTopology: true });
-    this.db = null;
-
-    this.connect(database);
+    MongoClient.connect(url, { useUnifiedTopology: true }, (err, client) => {
+      if (!err) {
+        // console.log('Connected successfully to server');
+        this.db = client.db(DB_DATABASE);
+        this.usersCollection = this.db.collection('users');
+        this.filesCollection = this.db.collection('files');
+      } else {
+        console.log(err.message);
+        this.db = false;
+      }
+    });
   }
 
-  async connect(database) {
-    try {
-      await this.client.connect();
-      this.db = this.client.db(database);
-      console.log('Connected to database');
-    } catch (err) {
-      console.error('Failed to connect to the database', err);
-      process.exit(1);
-    }
-  }
-
+  /**
+   * Checks if connection to Redis is Alive
+   * @return {boolean} true if connection alive or false if not
+   */
   isAlive() {
-    return this.client && this.client.isConnected() && this.db !== null;
+    return Boolean(this.db);
   }
 
+  /**
+   * Returns the number of documents in the collection users
+   * @return {number} amount of users
+   */
   async nbUsers() {
-    const users = this.db.collection('users');
-    return users.countDocuments();
+    const numberOfUsers = this.usersCollection.countDocuments();
+    return numberOfUsers;
   }
 
+  /**
+   * Returns the number of documents in the collection files
+   * @return {number} amount of files
+   */
   async nbFiles() {
-    const files = this.db.collection('files');
-    return files.countDocuments();
-  }
-
-  async findUser(user) {
-    const users = this.db.collection('users');
-    return users.findOne(user);
-  }
-
-  async createUser(user) {
-    const users = this.db.collection('users');
-    return users.insertOne(user);
-  }
-
-  async getUserByEmail(email) {
-    const users = this.db.collection('users');
-    return users.findOne({ email });
-  }
-
-  async findFile(file) {
-    const files = this.db.collection('files');
-    return files.findOne(file);
-  }
-
-  async createFile(file) {
-    const files = this.db.collection('files');
-    return files.insertOne(file);
-  }
-
-  async allFiles() {
-    const files = this.db.collection('files');
-    return files.find().toArray();
-  }
-
-  async readFile(fileId) {
-    const files = this.db.collection('files');
-    return files.findOne({ _id: new ObjectId(fileId) });
-  }
-
-  async deleteFile(fileId) {
-    const files = this.db.collection('files');
-    return files.deleteOne({ _id: new ObjectId(fileId) });
-  }
-
-  async saveFile(file) {
-    const files = this.db.collection('files');
-    return files.insertOne(file);
+    const numberOfFiles = this.filesCollection.countDocuments();
+    return numberOfFiles;
   }
 }
 
 const dbClient = new DBClient();
+
 export default dbClient;
